ba7606bbd906b483309fc49ae96f8f843cb2353b
Dave Mathews Njeru <41262970+davenjeru@users.noreply.github.com>
2025-09-21 19:13:18 +0300

    feat: Persist the contact us form (#10)

--- src/app/LandingPage/ContactUsSection/ContactForm/index.tsx
+++ src/app/LandingPage/ContactUsSection/ContactForm/index.tsx
@@ -1,9 +1,8 @@
+import { gql } from '@apollo/client';
+import { useMutation } from '@apollo/client/react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import {
-  type SubmitErrorHandler,
-  type SubmitHandler,
-  useForm,
-} from 'react-hook-form';
+import { type FC, type PropsWithChildren } from 'react';
+import { type SubmitHandler, useForm } from 'react-hook-form';
 import EmailInput from '@/app/LandingPage/ContactUsSection/ContactForm/EmailInput';
 import FullNameInput from '@/app/LandingPage/ContactUsSection/ContactForm/FullNameInput';
 import LinkedInInput from '@/app/LandingPage/ContactUsSection/ContactForm/LinkedInInput';
@@ -12,7 +11,116 @@ import {
   type FormSchema,
   formSchema,
 } from '@/app/LandingPage/ContactUsSection/ContactForm/schema.ts';
+import GradientButton from '@/lib/components/GradientButton';
+import LandingPageIcon from '@/lib/components/LandingPageIcon';
 import { Form } from '@/lib/components/ui/form';
+import closeIcon from '@/lib/icons/close.svg';
+import errorIcon from '@/lib/icons/error.svg';
+import errorBullet from '@/lib/icons/error-bullet.svg';
+import sentIcon from '@/lib/icons/sent.svg';
+
+const SEND_MESSAGE = gql`
+  mutation CreateReceivedMessage(
+    $email: String!
+    $fullName: String!
+    $linkedInProfile: String
+    $message: String!
+  ) {
+    createReceivedMessage(
+      input: {
+        email: $email
+        fullName: $fullName
+        linkedInProfile: $linkedInProfile
+        message: $message
+      }
+    ) {
+      success
+    }
+  }
+`;
+
+type ContactFormMessageProps = PropsWithChildren<{ close: () => void }>;
+
+const ContactFormMessage: FC<ContactFormMessageProps> = ({
+  close,
+  children,
+}) => (
+  <div
+    className={`
+      absolute top-4 right-4 bottom-4 left-4 z-30 flex flex-col items-center
+      gap-8 rounded-lg border border-gray-700 bg-background p-4
+    `}
+  >
+    <button
+      type="button"
+      onClick={close}
+      className={`cursor-pointer place-self-start`}
+    >
+      <LandingPageIcon src={closeIcon} alt="Close" width={24} height={24} />
+    </button>
+    {children}
+    <GradientButton
+      type="button"
+      className="absolute bottom-4 self-end"
+      onClick={close}
+    >
+      Send Another Message
+    </GradientButton>
+  </div>
+);
+
+const ContactFormError: FC<Pick<ContactFormMessageProps, 'close'>> = ({
+  close,
+}) => {
+  return (
+    <ContactFormMessage close={close}>
+      <div className="rounded-[50%] bg-[#1A283F] p-2">
+        <LandingPageIcon src={errorIcon} alt="Error" />
+      </div>
+      <h5 className="font-sora text-sora-text-large font-semibold text-red-500">
+        Oops! Something went wrong
+      </h5>
+      <p className="text-center font-roboto text-roboto-text-regular font-light">
+        We couldn't process your request at the moment.
+        <br />
+        Please check your connection and try again later.
+      </p>
+      <div
+        className={`
+          flex w-full flex-row justify-center gap-4 rounded-sm border
+          border-gray-700 bg-[#18152D] py-4 font-roboto text-roboto-text-small
+          text-red-500
+        `}
+      >
+        <LandingPageIcon src={errorBullet} alt="." height={8} width={8} />
+        <p>Error Code: 500</p>
+        <p>Server Unavailable</p>
+      </div>
+    </ContactFormMessage>
+  );
+};
+
+const ContactFormSuccess: FC<Pick<ContactFormMessageProps, 'close'>> = ({
+  close,
+}) => {
+  return (
+    <ContactFormMessage close={close}>
+      <div className="rounded-[50%] bg-[#1A283F] p-4">
+        <LandingPageIcon src={sentIcon} alt="Success" />
+      </div>
+      <h5
+        className={`font-sora text-sora-text-large font-semibold text-green-500`}
+      >
+        We have received your message
+      </h5>
+      <p className="text-center font-roboto text-roboto-text-regular font-light">
+        Thank you for reaching out.
+        <br />
+        We'll look at your message and get back to you.
+      </p>
+    </ContactFormMessage>
+  );
+};
 
 const ContactForm = () => {
   const form = useForm<FormSchema>({
@@ -21,19 +129,35 @@ const ContactForm = () => {
     mode: 'onBlur',
   });
 
-  const onSubmit: SubmitHandler<FormSchema> = (data) => console.log(data);
+  const [
+    sendMessage,
+    { loading: messageIsSending, error: sendMessageError, data, reset },
+  ] = useMutation(SEND_MESSAGE);
 
-  const onSubmitError: SubmitErrorHandler<FormSchema> = (data) =>
-    console.log(data);
+  type CreateReceivedMessageResponse = {
+    createReceivedMessage: { success: boolean };
+  };
+
+  let sendMessageSuccess = false;
+  if (data) {
+    const { createReceivedMessage } = data as CreateReceivedMessageResponse;
+    sendMessageSuccess = createReceivedMessage.success;
+  }
+  const onSubmit: SubmitHandler<FormSchema> = async (formData) => {
+    await sendMessage({ variables: formData });
+  };
 
   return (
     <div
       className={`
-        mt-8 flex w-128 flex-col items-center gap-4 rounded-md border
+        relative mt-8 flex w-128 flex-col items-center gap-4 rounded-lg border
         border-gray-700 p-4 pb-8
         [box-shadow:0px_25px_50px_-12px_rgba(239,77,172,0.25)]
       `}
     >
+      {sendMessageSuccess && <ContactFormSuccess close={() => reset()} />}
+      {sendMessageError && <ContactFormError close={() => reset()} />}
+
       <h2 className="self-start font-sora text-sora-heading-large font-light">
         Get In Touch
       </h2>
@@ -42,10 +166,7 @@ const ContactForm = () => {
         comes up
       </p>
       <Form {...form}>
-        <form
-          className="h-full w-full"
-          onSubmit={form.handleSubmit(onSubmit, onSubmitError)}
-        >
+        <form className="h-full w-full" onSubmit={form.handleSubmit(onSubmit)}>
           <div className="mt-4 flex flex-col gap-3">
             <div className="flex flex-row gap-2">
               <FullNameInput form={form} />
@@ -53,15 +174,13 @@ const ContactForm = () => {
             </div>
             <LinkedInInput form={form} />
             <MessageInput form={form} />
-            <button
-              className={`
-                h-11 w-35 cursor-pointer self-end rounded-[12px]
-                bg-linear-(--background-gradient)
-              `}
+            <GradientButton
               type="submit"
+              disabled={messageIsSending}
+              className="w-35 self-end"
             >
               Send
-            </button>
+            </GradientButton>
           </div>
         </form>
       </Form>
--- src/app/LandingPage/index.tsx
+++ src/app/LandingPage/index.tsx
@@ -1,3 +1,5 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 import CaseStudySection from '@/app/LandingPage/CaseStudySection.tsx';
 import ContactUsSection from '@/app/LandingPage/ContactUsSection';
 import LetUsProtectSection from '@/app/LandingPage/LetUsProtectSection';
@@ -5,15 +7,22 @@ import PrimarySection from '@/app/LandingPage/PrimarySection.tsx';
 import Footer from '@/lib/components/Footer';
 import NavBar from '@/lib/components/NavBar';
 
+const BACKEND_GRAPHQL_URL = `${import.meta.env.VITE_OSE_BACKEND_URI}/graphql`;
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: BACKEND_GRAPHQL_URL }),
+  cache: new InMemoryCache(),
+});
+
 const LandingPage = () => (
-  <>
+  <ApolloProvider client={client}>
     <NavBar />
     <PrimarySection />
     <CaseStudySection />
     <LetUsProtectSection />
     <ContactUsSection />
     <Footer />
-  </>
+  </ApolloProvider>
 );
 
 export default LandingPage;
